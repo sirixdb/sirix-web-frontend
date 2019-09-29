ee7a6c6389f999f72bfbc2fe7f59f5fbb2a3dfb0
Your Name <27681135@qq.com>
2019-09-29 09:30:20 +0800

    add element-ui lib. add  /table route. add a tableView demo.

---
+++ index.d.ts
@@ -0,0 +1,3 @@
+declare module 'element-ui/lib/locale/lang/en' {
+  import locale = require('element-ui/lib/locale/lang/en');
+}
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import router from './router';
 import store from './store';
 import './registerServiceWorker';
 
+// add element-ui lib
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import locale from 'element-ui/lib/locale/lang/en';
+Vue.use(ElementUI, { locale });
+
 Vue.config.productionTip = false;
 
 new Vue({
