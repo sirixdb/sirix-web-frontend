b456c0dce220e101a074ecff35d15d2b5293a860
Johannes Lichtenberger <johannes.lichtenberger@uni-konstanz.de>
2019-10-29 16:45:09 +0100

    Create callback.vue

---
+++ pages/callback.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>You are Secure</h1>
+    <el-link type="primary"><el-button type="success">Callback</el-button></el-link>
+  </div>
+</template>
+<script>
+export default {
+    middleware: ['auth']
+}
+</script>
+<style scoped>
+</style>
