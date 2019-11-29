86545f85e8fa4b2119e02e9c9325ddbbf4e5cb59
Johannes Lichtenberger <johannes.lichtenberger@unitedplanet.de>
2019-11-30 00:07:14 +0100

    Remove about page, add database page...

--- pages/callback.vue
+++ pages/callback.vue
@@ -1,11 +1,27 @@
 <template>
   <div>
-    <h1>You are logged in</h1>
+    <h1 v-if="loggedIn()">You are logged in.</h1>
+    <h1 v-else="!loggedIn()">You are not logged in.</h1>
   </div>
 </template>
 <script>
 export default {
-    middleware: ['auth']
+    props: [
+      'expanded'
+    ],
+
+    middleware: ['auth'],
+
+    methods: {
+    	loggedIn() {
+    	    return this.$auth.loggedIn
+    	},
+
+        async logout() {
+            await this.$auth.logout()
+            this.$router.push('/login')
+        }
+    }
 }
 </script>
 <style scoped>
---
+++ pages/databases/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="databases">
+    <h3>Databases</h3>
+    <el-tree :data="databases" :props="defaultProps"></el-tree>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class TreeView extends Vue {
+  created() {
+    console.log("begin");
+    this.getDatabases().then(databases => {
+      this.databases = databases;
+    });
+    this.databases.forEach(element => {
+      for (let database in element.keys) {
+        this.getResources(database);
+      }
+    });
+  };
+
+  private databases: Array<Map<string, string>> = [];
+  private defaultProps = this.databases;
+
+  private getDatabases(): Promise<Array<Map<string, string>>> {
+    return this.$axios
+      .$get("/")
+      .then((res: any) => {
+        return Promise.resolve(res.data);
+      })
+      .catch(() => {
+        return 0;
+      });
+  }
+
+  private getResources(database: string) {
+    this.$axios
+      .$get(`sirix/${database}`)
+      .then((res: any) => {
+        console.log(res.data);
+      })
+      .catch(() => {
+        return 0;
+      });
+  }
+}
+</script>
