290bbeee2d3a492defd6a90a684210dd84ee6a87
Johannes Lichtenberger <johannes.lichtenberger@unitedplanet.de>
2019-12-07 15:44:52 +0100

    Add file upload component

---
+++ components/FileUpload.vue
@@ -0,0 +1,24 @@
+<template>
+  <dropzone id="foo" ref="el" :options="options" :destroyDropzone="true"></dropzone>
+</template>
+
+<script>
+import Dropzone from 'nuxt-dropzone'
+import 'nuxt-dropzone/dropzone.css'
+
+export default {
+  components: {
+    Dropzone
+  },
+  data() {
+    return {
+      // See https://rowanwins.github.io/vue-dropzone/docs/dist/index.html#/props
+      options: {},
+    }
+  },
+  mounted() {
+    // Everything is mounted and you can access the dropzone instance
+    const instance = this.$refs.el.dropzone
+  }
+}
+</script>
